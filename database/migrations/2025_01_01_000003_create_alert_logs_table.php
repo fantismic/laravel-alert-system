37320a78fa9af89cfa5b95a97df8f9ae2918f051
Martin Picard Smith <mpicardsmith@teco.com.ar>
2025-07-07 20:15:49 -0300

    Corregir la eliminación de la tabla alert_logs en la migración de creación de registros de alertas

--- database/migrations/2025_01_01_000003_create_alert_logs_table.php
+++ database/migrations/2025_01_01_000003_create_alert_logs_table.php
@@ -22,6 +22,6 @@ return new class extends Migration {
     }
 
     public function down(): void {
-        Schema::dropIfExists('alert_recipients');
+        Schema::dropIfExists('alert_logs');
     }
 };
