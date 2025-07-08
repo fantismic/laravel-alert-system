672c987040ae5086a4192095c2f26726d43b409b
Martin Picard Smith <mpicardsmith@teco.com.ar>
2025-07-08 14:19:38 -0300

    Eliminar la función sendErrorAlert de helpers.php

--- helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('sendErrorAlert')) {
-    function sendErrorAlert(string $type, string $message, array $details = []) {
-        app()->booted(function () use ($type, $message, $details) {
-            app('alert-system')->send($type, $message, $details);
-        });
-    }
-}
