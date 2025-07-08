34718726f924af6c3def6227264cf26306beb081
Martin Picard Smith <mpicardsmith@teco.com.ar>
2025-07-07 22:19:54 -0300

    Mejorar la configuración de Telegram en el sistema de alertas y actualizar el servicio para usar GuzzleHttp

--- config/alert-system.php
+++ config/alert-system.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'telegram' => [
+        'token' => env('TELEGRAM_BOT_TOKEN'),
+        'proxy' => env('TELEGRAM_PROXY',null),
+    ],
     'envs' => ["production"],
     'layout' => 'layouts.app',
     'db-history' => true,
--- src/Services/TelegramService.php
+++ src/Services/TelegramService.php
@@ -2,27 +2,39 @@
 
 namespace Fantismic\AlertSystem\Services;
 
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class TelegramService
 {
-    protected string $botToken;
+    protected Client $client;
+    protected string $token;
 
     public function __construct()
     {
-        $this->botToken = config('services.telegram-bot-api.token');
+        $this->token = config('alert-system.telegram.token');
+
+        $guzzleConfig = [
+            'base_uri' => "https://api.telegram.org/bot{$this->token}/",
+            'timeout'  => 5.0,
+            'verify'   => false
+        ];
+
+        // Optional proxy support
+        if ($proxy = config('alert-system.telegram.proxy')) {
+            $guzzleConfig['proxy'] = $proxy;
+        }
+
+        $this->client = new Client($guzzleConfig);
     }
 
-    public function sendMessage(string $chatId, string $message): bool
+    public function sendMessage(string $chatId, string $message): void
     {
-        $url = "https://api.telegram.org/bot{$this->botToken}/sendMessage";
-
-        $response = Http::post($url, [
-            'chat_id' => $chatId,
-            'text' => $message,
-            'parse_mode' => 'HTML',
+        $this->client->post('sendMessage', [
+            'form_params' => [
+                'chat_id' => $chatId,
+                'text' => $message,
+                'parse_mode' => 'HTML',
+            ],
         ]);
-
-        return $response->ok();
     }
 }
